4f37fca2a6b171740dd1c7ebbaaffdf870ae72f4
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-16 19:15:17 +0100

    feat: añamdimos botón de acción al dashboard screen

--- app/src/main/java/com/proyecto/recetasencasa/dashboard/presentation/screens/DashboardScreen.kt
+++ app/src/main/java/com/proyecto/recetasencasa/dashboard/presentation/screens/DashboardScreen.kt
@@ -1,8 +1,11 @@
 package com.proyecto.recetasencasa.dashboard.presentation.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,5 +31,11 @@ fun DashboardScreen(
 
         Text(text = "Recetas: $totalRecipes")
         Text(text = "Planificadas: $plannedMeals")
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(onClick = onViewRecipesClick){
+            Text(text = "Ver recetas")
+        }
     }
 }
