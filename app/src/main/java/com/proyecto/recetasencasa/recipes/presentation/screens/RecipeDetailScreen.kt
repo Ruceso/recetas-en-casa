503bbbd5b0bae7735bbfcb4f64cf820bd770c829
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-16 20:09:39 +0100

    feat: añadimos preview a recipe detail

--- app/src/main/java/com/proyecto/recetasencasa/recipes/presentation/screens/RecipeDetailScreen.kt
+++ app/src/main/java/com/proyecto/recetasencasa/recipes/presentation/screens/RecipeDetailScreen.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlin.String
 
 @Composable
 fun RecipeDetailScreen(
@@ -48,5 +51,24 @@ fun RecipeDetailScreen(
         ingredients.forEach { ingredient ->
             Text(text = "- $ingredient")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RecipeDetailScreenPreview() {
+    Surface{
+        RecipeDetailScreen(
+            name = "Pasta carbonara",
+            time = 20,
+            difficulty = "Fácil",
+            description = "Receta clásica italiana...",
+            ingredients = listOf(
+                "Pasta",
+                "Huevo",
+                "Queso",
+                "Bacon"
+            )
+        )
     }
 }
