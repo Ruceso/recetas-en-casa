b6430bc6a24121ea1f4da1b6a0635b2715c5d4cc
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-16 19:00:09 +0100

    feat: añadimos layout básico al dashboard screen

--- app/src/main/java/com/proyecto/recetasencasa/dashboard/presentation/screens/DashboardScreen.kt
+++ app/src/main/java/com/proyecto/recetasencasa/dashboard/presentation/screens/DashboardScreen.kt
@@ -1,8 +1,24 @@
 package com.proyecto.recetasencasa.dashboard.presentation.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DashboardScreen() {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        Text(
+            text = "Dashboard",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
 }
