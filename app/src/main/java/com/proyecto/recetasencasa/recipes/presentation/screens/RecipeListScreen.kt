8687e89d5f2e9b719411ee0ed8aa56a8e51c2703
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-16 19:33:18 +0100

    feat: añadimos recipe list screen base

---
+++ app/src/main/java/com/proyecto/recetasencasa/recipes/presentation/screens/RecipeListScreen.kt
@@ -0,0 +1,11 @@
+package com.proyecto.recetasencasa.recipes.presentation.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecipeListScreen(
+    recipes: List<String>,
+    onRecipeClick: (String) -> Unit
+){
+
+}
