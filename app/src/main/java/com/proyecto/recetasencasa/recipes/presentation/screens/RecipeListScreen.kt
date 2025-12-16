cdc0161cd44d725c893df47522feae3cfde17869
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-16 19:41:33 +0100

    feat: añadimos layout a recipe list

--- app/src/main/java/com/proyecto/recetasencasa/recipes/presentation/screens/RecipeListScreen.kt
+++ app/src/main/java/com/proyecto/recetasencasa/recipes/presentation/screens/RecipeListScreen.kt
@@ -1,11 +1,34 @@
 package com.proyecto.recetasencasa.recipes.presentation.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun RecipeListScreen(
     recipes: List<String>,
     onRecipeClick: (String) -> Unit
 ){
-
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(recipes) { recipe ->
+            Text(
+                text = recipe,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .clickable{onRecipeClick(recipe)}
+            )
+        }
+    }
 }
