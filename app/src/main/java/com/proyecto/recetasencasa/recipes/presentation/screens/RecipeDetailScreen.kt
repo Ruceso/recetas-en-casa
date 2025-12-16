1f64abbd4df9fb849f5ca12fa90b1bc4243b6965
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-16 19:49:32 +0100

    feat: añadimos recipe detail screen base

---
+++ app/src/main/java/com/proyecto/recetasencasa/recipes/presentation/screens/RecipeDetailScreen.kt
@@ -0,0 +1,14 @@
+package com.proyecto.recetasencasa.recipes.presentation.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun recipeDetailScreen(
+    name: String,
+    time: Int,
+    difficculty: String,
+    description: String,
+    ingredients: List<String>
+){
+
+}
