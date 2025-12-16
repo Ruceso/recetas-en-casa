d7b266b77a1bbf98e5c016f35a64a128e3c2e050
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-16 20:36:15 +0100

    feat: añadimos create recipe screen base

---
+++ app/src/main/java/com/proyecto/recetasencasa/recipes/presentation/screens/CreateRecipeScreen.kt
@@ -0,0 +1,16 @@
+package com.proyecto.recetasencasa.recipes.presentation.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CreateRecipeScreen(
+    name: String,
+    description: String,
+    time: String,
+    onNameChange: (String) -> Unit,
+    onDescriptionChange: (String) -> Unit,
+    onTimeChange: (String) -> Unit,
+    onSaveClick: () -> Unit
+){
+
+}
