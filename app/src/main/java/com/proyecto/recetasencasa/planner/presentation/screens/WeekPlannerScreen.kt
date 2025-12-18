2c8b233a1193dcdf6c4ae4d31db7404bd2bdfa71
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-18 11:58:24 +0100

    feat: añadimos week planner screen base

---
+++ app/src/main/java/com/proyecto/recetasencasa/planner/presentation/screens/WeekPlannerScreen.kt
@@ -0,0 +1,11 @@
+package com.proyecto.recetasencasa.planner.presentation.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WeekPlannerScreen(
+    plan: Map<String, String>,
+    onDayClick: (String) -> Unit
+){
+
+}
