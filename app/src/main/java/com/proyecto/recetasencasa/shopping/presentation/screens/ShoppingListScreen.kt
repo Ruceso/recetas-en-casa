864695342e97ca0c53cde220906dd1eb98210bf6
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-18 12:14:46 +0100

    feat: añadimos shopping list screen base

---
+++ app/src/main/java/com/proyecto/recetasencasa/shopping/presentation/screens/ShoppingListScreen.kt
@@ -0,0 +1,11 @@
+package com.proyecto.recetasencasa.shopping.presentation.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ShoppingListScreen(
+    items: List<String>,
+    onItemClick: (String) -> Unit
+){
+
+}
