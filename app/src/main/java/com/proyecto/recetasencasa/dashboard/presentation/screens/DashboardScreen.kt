220fc65ab339b949695b08326bbbedf01e018d9d
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-19 15:33:27 +0100

    feat: añadimos funciones Header y DashboardStatItem en Dashboard

--- app/src/main/java/com/proyecto/recetasencasa/dashboard/presentation/screens/DashboardScreen.kt
+++ app/src/main/java/com/proyecto/recetasencasa/dashboard/presentation/screens/DashboardScreen.kt
@@ -1,12 +1,19 @@
 package com.proyecto.recetasencasa.dashboard.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.MenuBook
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,12 +22,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.proyecto.recetasencasa.core.presentation.components.BottomNavigationBar
+import androidx.compose.material.icons.filled.Restaurant
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
+import com.proyecto.recetasencasa.core.presentation.components.Header
+import com.proyecto.recetasencasa.dashboard.presentation.components.DashboardStatItem
+
 
 @Composable
 fun DashboardScreen(
     userName: String,
     totalRecipes: Int,
     plannedMeals: Int,
+    ingredients: Int,
+    recipesFav: Int,
     onViewRecipesClick: () -> Unit
 ) {
     Scaffold(
@@ -41,22 +56,66 @@ fun DashboardScreen(
                 .padding(innerPadding)
                 .padding(16.dp)
         ) {
-            Text(
-                text = "Hola, $userName",
-                style = MaterialTheme.typography.headlineMedium
+            Header(
+                icon = Icons.Default.Restaurant,
+                title = "Las recetas de $userName",
+                modifier = Modifier.padding(bottom = 24.dp)
             )
 
-            Text(text = "Recetas: $totalRecipes")
-            Text(text = "Planificadas: $plannedMeals")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                DashboardStatItem(
+                    icon = Icons.AutoMirrored.Outlined.MenuBook,
+                    label = "Recetas",
+                    value = totalRecipes,
+                    modifier = Modifier.weight(1f)
+                )
+
+                DashboardStatItem(
+                    icon = Icons.Default.DateRange,
+                    label = "Planificar",
+                    value = plannedMeals,
+                    modifier = Modifier.weight(1f)
+                )
+
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                DashboardStatItem(
+                    icon = Icons.Default.ShoppingCart,
+                    label = "Compra",
+                    value = ingredients,
+                    modifier = Modifier.weight(1f)
+                )
+
+                DashboardStatItem(
+                    icon = Icons.Default.Favorite,
+                    label = "Favoritas",
+                    value = recipesFav,
+                    modifier = Modifier.weight(1f)
+                )
+
+            }
 
             Spacer(modifier = Modifier.height(24.dp))
 
             Button(onClick = onViewRecipesClick) {
-                Text(text = "Ver recetas")
+                    Text(text = "Ver recetas")
             }
+
+        }
         }
     }
-}
 
 @Preview(showBackground = true)
 @Composable
@@ -66,6 +125,8 @@ fun DashboardScreenPreview() {
             userName = "Ruben",
             totalRecipes = 12,
             plannedMeals = 8,
+            ingredients = 18,
+            recipesFav = 12,
             onViewRecipesClick = {}
         )
     }
