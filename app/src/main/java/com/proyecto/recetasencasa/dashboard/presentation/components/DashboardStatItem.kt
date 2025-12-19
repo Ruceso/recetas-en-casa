229d92f6d22fc27fa7c67398766e21239040e1f0
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-19 13:45:07 +0100

    feat: añadimos carpeta components y botones de navegación

---
+++ app/src/main/java/com/proyecto/recetasencasa/dashboard/presentation/components/DashboardStatItem.kt
@@ -0,0 +1,57 @@
+package com.proyecto.recetasencasa.dashboard.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.MenuBook
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.proyecto.recetasencasa.dashboard.presentation.screens.DashboardScreen
+
+@Composable
+fun DasboardStatItem(
+    icon: ImageVector,
+    label: String,
+    text: String,
+    value: Int,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Icon(
+            imageVector = icon,
+            contentDescription = label,
+            tint = MaterialTheme.colorScheme.primary
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(text = "$text: $value")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DashboardStatItemPreview(){
+    Surface {
+        DasboardStatItem(
+            icon = Icons.AutoMirrored.Outlined.MenuBook,
+            label = "Recetas",
+            text = "Recetas",
+            value = 12,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
