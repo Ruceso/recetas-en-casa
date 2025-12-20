06513f338e207f765986d24618ca94545b7614ca
Ruben Cervera Soria <rucerso@gmail.com>
2025-12-20 16:44:12 +0100

    feat: creamos función del BackHeader

--- app/src/main/java/com/proyecto/recetasencasa/recipes/presentation/components/BackHeader.kt
+++ app/src/main/java/com/proyecto/recetasencasa/recipes/presentation/components/BackHeader.kt
@@ -1,2 +1,52 @@
 package com.proyecto.recetasencasa.recipes.presentation.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BackHeader(
+    title: String,
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.ArrowBackIosNew,
+            "Volver",
+            modifier = Modifier
+                .clickable { onBackClick() }
+                .padding(end = 8.dp)
+        )
+
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BackHeaderPreview(){
+    Surface {
+        BackHeader(
+            title = "Nueva receta",
+            onBackClick = {}
+        )
+    }
+}
